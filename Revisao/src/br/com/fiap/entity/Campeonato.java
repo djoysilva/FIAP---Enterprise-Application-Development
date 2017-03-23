5f0d65062396028433b48e79fba28631e0c3d8ca
logonrm <logonrm@fiap.com.br>
2017-03-23 20:59:32 -0300

    Add nem web service project with querys in bd

--- Revisao/src/br/com/fiap/entity/Campeonato.java
+++ Revisao/src/br/com/fiap/entity/Campeonato.java
@@ -1,8 +1,21 @@
 package br.com.fiap.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@SequenceGenerator(name="seqCampeonato", sequenceName="SQ_TB_CAMPEONATO", allocationSize=1)
+@Table(name = "TB_CAMPEONATO")
 public class Campeonato {
-
+	
+	@Id
+	@Column(name="CD_CAMPEONATO", nullable=false)
+	private int codigo;
+	
+	@Column(name="NM_CAMPEONATO")
+	private String nome;
+	
 }
--- Revisao/src/br/com/fiap/entity/Tecnico.java
+++ Revisao/src/br/com/fiap/entity/Tecnico.java
@@ -1,8 +1,24 @@
 package br.com.fiap.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@SequenceGenerator(name="seqTecnico", sequenceName="SQ_TB_TECNICO", allocationSize=1)
+@Table(name = "TB_TECNICO")
 public class Tecnico {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="seqTecnico")
+	@Column(name="CD_TECNICO", nullable=false)
+	private int codigo;
+	
+	@Column(name="NM_TECNICO")
+	private String nome;
+	
 }
