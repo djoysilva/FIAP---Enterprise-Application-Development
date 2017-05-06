5ea1bfdf900fd85c746255cf2c2d1c5f7fd352bc
logonrm <logonrm@fiap.com.br>
2017-05-05 22:48:06 -0300

    xhtml with crud

--- 19-JSF-JPA/src/br/com/fiap/bean/ListaAtletaBean.java
+++ 19-JSF-JPA/src/br/com/fiap/bean/ListaAtletaBean.java
@@ -1,53 +1,50 @@
 package br.com.fiap.bean;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import org.apache.jasper.tagplugins.jstl.core.ForEach;
+
 import br.com.fiap.bo.AtletaBO;
 import br.com.fiap.entity.Atleta;
-import br.com.fiap.exception.DBException;
 
 @ManagedBean
-public class AtletaBean {
-
-	private Atleta atleta;
+public class ListaAtletaBean {
+	private List<Atleta> atletas;
 
 	private AtletaBO bo;
 	
 	@PostConstruct
 	private void init(){
-		atleta = new Atleta();
-		bo = new AtletaBO();
+		bo= new AtletaBO();
+		atletas = bo.listar();
 	}
 	
-	public String cadastrar(){
+	public String excluir(int codigo){
 		FacesMessage msg;
-		try {
-			if(atleta.getCodigo() == 0){
-				bo.cadastrar(atleta);
-				msg = new FacesMessage("Atleta cadastrado!");
-			}else{
-				bo.atualizar(atleta);
-				msg = new FacesMessage("Atleta atualizado!");
-			}	
-		} catch (DBException e) {
+		try{
+			bo.remover(codigo);
+			msg = new FacesMessage("Excluido!");
+		}catch (Exception e) {
 			e.printStackTrace();
 			msg = new FacesMessage("Erro");
-			return "atleta";
 		}
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
 		return "lista-atleta?faces-redirect=true";
 	}
 	
-	public Atleta getAtleta() {
-		return atleta;
+	public List<Atleta> getAtletas() {
+		return atletas;
 	}
 
-	public void setAtleta(Atleta atleta) {
-		this.atleta = atleta;
+	public void setAtletas(List<Atleta> atletas) {
+		this.atletas = atletas;
 	}
+	
 	
 }
